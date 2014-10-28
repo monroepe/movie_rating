95fb8a03c95dd32fad30f84a306009c819f2600a
Peter <monroepe@gmail.com>
2014-10-28 13:47:13 -0400

    Functions properly if movie exists

--- movie_rating.rb
+++ movie_rating.rb
@@ -3,11 +3,16 @@ require "csv"
 require "rest_client"
 require "json"
 
-movies_years = CSV.read("movies.csv")
-movies_ratings = []
+movies_years = CSV.read("short_movies.csv")
+movies_ratings = {}
 
 movies_years.each do |movie|
-  movies_ratings << RestClient.get("http://www.omdbapi.com/?t=#{movie[0].split(" ").join("+")}&y=#{movie[1]}")
-  binding.pry
+  movies_ratings[movie[0]] = JSON.parse(RestClient.get("http://www.omdbapi.com/?t=#{movie[0].split(" ").join("+")}&y=#{movie[1]}"))["imdbRating"].to_f
 end
 
+CSV.open("movies_with_ratings.csv", "w") do |csv|
+  movies_ratings.sort_by{ |_k, v| -v }.each do |movie|
+    puts "#{movie[0]} -- #{movie[1]}"
+    csv << [movie[0], movie[1]]
+  end
+end
