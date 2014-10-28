f9e732b0524e05ee5157e6a093a711dd3de911d5
Peter <monroepe@gmail.com>
2014-10-28 15:24:04 -0400

    Now in descending order

--- movie_rating.rb
+++ movie_rating.rb
@@ -7,19 +7,21 @@ movies_years = CSV.read("short_movies.csv")
 movies_ratings = {}
 
 movies_years.each do |movie|
-  url = URI.encode("http://www.omdbapi.com/?t=#{movie[0].split(" ").join("+")}&y=#{movie[1]}")
+  url = URI.encode("http://www.omdbapi.com/?t=#{ movie[0].split(" ").join("+") }&y=#{ movie[1] }")
   response = JSON.parse(RestClient.get(url))
 
-  if response["imdbRating"]
+  if response["imdbRating"] && response["imdbRating"] != "N/A"
     movies_ratings[movie[0]] = response["imdbRating"].to_f
   else
-    movies_ratings[movie[0]] = "N/A"
+    movies_ratings[movie[0]] = -1
   end
 end
 
 CSV.open("movies_with_ratings.csv", "w") do |csv|
-  movies_ratings.each do |movie|
-    puts "#{movie[0]} -- #{movie[1]}"
+  movies_ratings.sort_by{ |_k, v| -v }.each do |movie|
+    movie[1] = "N/A" if movie[1] == -1
+
+    puts "#{ movie[0] } -- #{ movie[1] }"
     csv << [movie[0], movie[1]]
   end
 end
